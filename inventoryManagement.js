2882014267ffca296eeff69d37d0d2ed0c67ba96
joshua-odero <misikojodero@gmail.com>
2025-11-24 15:59:28 +0300

    Modifying inventoryManagement.js with array functions

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,34 @@
-// Write your code here
+//Array definition
+const products = ["Laptops","Phones","Headphones", "Monitor"];
 
+//Function to log the first product in array
+function logFirstProduct(){
+
+  console.log(products[0]);
+
+}
+
+//Function to add new product in array
+function addProduct(productName){
+
+  products.push(productName);
+
+}
+
+//Function to update the name of the product
+function updateProductName(arrayPosition,newProductName){
+
+  products[arrayPosition] = newProductName;
+
+}
+
+
+//Function to remove the last product from the array
+function removeLastProduct() {
+
+  products.pop();
+  
+}
 
 
 // Export the necessary parts for testing
